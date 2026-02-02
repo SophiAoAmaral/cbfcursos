90e899742399cdcb2a53bafe0a278e98b440d35f
SophiAoAmaral <167262627+SophiAoAmaral@users.noreply.github.com>
2026-02-02 20:10:09 -0300

    opeado ternario

--- JavaScript/aula02/script.js
+++ JavaScript/aula02/script.js
@@ -1,5 +1,16 @@
-"use strict";
+
 
 let n = 10;
 
 console.log(n);
+
+console.log(n++)
+console.log(n);
+
+console.log(++n);
+
+let num = 10;
+
+res = (!(num%2) ? 'Par' : 'Impar');
+
+console.log(res)
