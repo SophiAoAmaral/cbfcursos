34576a1f884d89d34ef9f3b7841f7377b6f0527d
SophiAoAmaral <167262627+SophiAoAmaral@users.noreply.github.com>
2026-02-04 23:38:50 -0300

    LOOPS

--- JavaScript/aula02/script.js
+++ JavaScript/aula02/script.js
@@ -66,7 +66,7 @@ if(num <10){
 };
 
 
-let idade = 55;
+let idade ;
 
 if(idade <18){
     console.log(`Sua idade é ${idade} e vc não pode votar`)
@@ -77,3 +77,70 @@ if(idade <18){
 }
 
 //SWITCH CASE
+let colocacao = 20;
+
+switch(colocacao){
+    case 1:
+        console.log('Primeiro Lugar');
+        break
+    case 2 :
+      console.log('Segundo Lugar');
+      break
+    case 3:
+      console.log('Terceiro Lugar');
+      break
+      default:
+      console.log('Nao Subiu');
+      break
+
+
+
+}
+
+
+//FOR
+
+//for(let i=0; i<100; i++){
+    //if(i%2 == 0){
+      //  console.log(i + ' Par')
+   // }else{
+    //    console.log(i + ' Impar')
+ //   }
+//}
+
+//let numero = [10,10,30,40,50];
+
+//for(n in numero){
+    ///console.log(numero[n])
+//}
+
+
+
+//const objetos = document.getElementsByTagName('div');
+
+//for (o of objetos){
+    //console.log(o)
+//}
+
+//for (o in objetos){
+  //  console.log(o) 
+//}
+
+
+//WHILE
+let m = 0;
+while(m<10){
+    console.log(m)
+    m++
+}
+
+
+
+let p = 5;
+let fatorial = 1;
+
+while(p >= 1){
+    fatorial*=p
+    n--
+}
+
